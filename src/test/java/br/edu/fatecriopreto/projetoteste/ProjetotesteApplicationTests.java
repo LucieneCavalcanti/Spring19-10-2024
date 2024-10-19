accd26e83495214619f54ac5da906c05881dfdac
Professora Luciene Cavalcanti <lucienecavalcanti@live.com>
2024-10-19 15:27:38 -0300

    Add files via upload

---
+++ src/test/java/br/edu/fatecriopreto/projetoteste/ProjetotesteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.fatecriopreto.projetoteste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetotesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
